399358b5115fe8abffcb56f641aa8e704364ad98
Praharsh Paravastu <paravastupraharsh99@gmail.com>
2020-08-11 03:25:36 +0530

    Added the resume download feature

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const arrowBtn = document.getElementById("arrow");
 const projects = document.getElementById("projects");
+const resumeDownloadBtn = document.getElementById("resume-download");
 
+// Event Listeners
 arrowBtn.onclick = () => {
   projects.getAttribute("style")
     ? projects.removeAttribute("style")
@@ -9,12 +11,20 @@ arrowBtn.onclick = () => {
   projects.classList.toggle("move-up");
 };
 
+resumeDownloadBtn.onclick = () => {
+  const a = document.createElement("a");
+  a.href = "/utils/Resume.pdf";
+  a.download = "Praharsh's Resume";
+};
+
+// Utility function
 const sleep = (ms) => {
   return new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
 };
 
+// removing the loader after 3 seconds
 window.onload = () => {
   const loaderContainer = document.getElementById("loader-container");
 
